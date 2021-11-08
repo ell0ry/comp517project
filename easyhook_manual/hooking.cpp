232619e8fcecbb0f69da5856308f6d4538f52a96
luke_atayde <lucas@DESKTOP-RRSS5S1>
2021-11-07 20:01:30 -0600

    Adding a dll for the hooking logic

--- easyhook_manual/hooking.cpp
+++ easyhook_manual/hooking.cpp
@@ -7,6 +7,7 @@
 
 
 using namespace std;
+
 // Hook Defs
 void WINAPI myDiskFreespaceHook(LPCSTR lpRootPathName, LPDWORD lpSectors, LPDWORD lpBytesPerSector, LPDWORD freeCluster, LPDWORD lpTotalClusters);
 std::function<void(LPCSTR, LPDWORD, LPDWORD, LPDWORD, LPDWORD)> Disk_hook = 
@@ -197,45 +198,11 @@ list<HookInstaller> CreateHooks() {
     WindowsAPIHelper windowsHelper;
 
     //add specific hooks
-    //hookList.push_back(CreateHookingEnvironment(windowsHelper._DiskFreeSpace, myDiskFreespaceHook, &Disk_hook));
     hookList.push_back(CreateHookingEnvironment(windowsHelper._CreateFile, myCreateFileW, &create_file));
-    //hookList.push_back(CreateHookingEnvironment("sds", myDiskFreespaceHook, &Disk_hook));
     return hookList;
 }
 // ======================================================  Hooking Helper Classes ============================================== //
 
-
-
-void disk_space() {
-
-    DWORD sectors = 0;
-    DWORD bps = 0;
-    DWORD free_cluster = 0;
-    DWORD total_cluster = 0;
-    GetDiskFreeSpaceA("C:/", &sectors, &bps, &free_cluster, &total_cluster);
-}
-
-
-void simple_disk_hooking_example() {
-    cout << "Calling Free Disk Space Before\n";
-    disk_space();
-
-    list<HookInstaller> hooks = CreateHooks();
-    for (HookInstaller hk : hooks) {
-        hk.enableHookForCurrentProcess();
-    }
-    cout << "Calling Free Disk Space After hook install\n";
-    disk_space();
-    CreateFileW(NULL, 0, 0, NULL, 0, 0, nullptr);
-
-    for (HookInstaller hk : hooks) {
-        hk.uninstallHook();
-    }
-    cout << "Calling Free Disk Space After hook remove\n";
-    disk_space();
-    cout << "\n\nRestore ALL entry points of pending removals issued by LhUninstallHook()\n";
-    LhWaitForPendingRemovals();
-}
 
 void HookProcess(DWORD processToHook) {
     cout << "hooking into " << processToHook << "\n";
@@ -243,6 +210,7 @@ void HookProcess(DWORD processToHook) {
     for (HookInstaller hook : hookEnv) {
         hook.enableHookForProccess(processToHook);
     }
+
     cout << "Press enter to unhook and exit\n";
     cin.ignore();
     cin.get();
