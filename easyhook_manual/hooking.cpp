c10fb4242c157d23e73e6d1bb7d7973fd458b11f
luke_atayde <lucas@DESKTOP-RRSS5S1>
2021-11-07 20:37:00 -0600

    Added code to inject hooking_dll

--- easyhook_manual/hooking.cpp
+++ easyhook_manual/hooking.cpp
@@ -228,6 +228,34 @@ int main()
     cout << "Enter pid of application to hook:\n";
     cin >> line;
     DWORD pidOfInterest = atol(line);
-    HookProcess(pidOfInterest);
+    // HookProcess(pidOfInterest);
+
+    WCHAR dllName[] = L"hooking_dll.dll";
+
+    NTSTATUS nt = RhInjectLibrary(
+        pidOfInterest,
+        0,
+        EASYHOOK_INJECT_DEFAULT,
+        NULL,
+        dllName,
+        NULL,
+        0
+    );
+
+    if (nt != 0)
+    {
+        printf("RhInjectLibrary failed with error code = %d\n", nt);
+        PWCHAR err = RtlGetLastErrorString();
+        std::wcout << err << "\n";
+    }
+    else
+    {
+        std::wcout << L"Library injected successfully.\n";
+    }
+
+    std::wcout << "Press Enter to exit";
+    std::wstring input;
+    std::getline(std::wcin, input);
+    std::getline(std::wcin, input);
     return 0;
 }
